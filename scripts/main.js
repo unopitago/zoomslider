5d72f0273804cb29387383ed92829252928908df
Dung Nguyen <ntdung226@gmail.com>
2019-01-04 21:03:03 +0700

    Add files via upload
    a

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+$('.show').zoomImage();
+$('.show-small-img:first-of-type').css({ 'border': 'solid 1px #951b25', 'padding': '2px' })
+$('.show-small-img:first-of-type').attr('alt', 'now').siblings().removeAttr('alt')
+$('.show-small-img').click(function () {
+  $('#show-img').attr('src', $(this).attr('src'))
+  $('#big-img').attr('src', $(this).attr('src'))
+  $(this).attr('alt', 'now').siblings().removeAttr('alt')
+  $(this).css({ 'border': 'solid 1px #951b25', 'padding': '2px' }).siblings().css({ 'border': 'none', 'padding': '0' })
+})
